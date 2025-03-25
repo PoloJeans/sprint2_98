6b5c2c70742dc148fa67307fc91090e9d37a4a5f
Joshua Luna-Li <lunalijoshua1@gmail.com>
2025-03-24 23:42:41 -0400

    Increased Performane

--- main.py
+++ main.py
@@ -37,6 +37,6 @@ while running:
     # flip() the display to put your work on screen
     pygame.display.flip()
 
-    clock.tick(60)  # limits FPS to 60
+    clock.tick(120)  # limits FPS to 60
 
 pygame.quit()
