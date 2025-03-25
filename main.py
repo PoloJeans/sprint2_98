4b176ef9e02cf95dc0907b2a5e771387b296d0df
PoloJeans <dylanknguyen04@gmail.com>
2025-03-24 23:19:29 -0400

    Mi circle desu

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ pygame.init()
 screen = pygame.display.set_mode((1280, 720))
 clock = pygame.time.Clock()
 running = True
+dt = 0.01
+
+player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
 
 while running:
     # poll for events
@@ -15,10 +18,20 @@ while running:
             running = False
 
     # fill the screen with a color to wipe away anything from last frame
-    screen.fill("purple")
+    screen.fill("pink")
 
     # RENDER YOUR GAME HERE
+    pygame.draw.circle(screen, "green", player_pos, 40)
 
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_w]:
+        player_pos.y -= 300 * dt
+    if keys[pygame.K_s]:
+        player_pos.y += 300 * dt
+    if keys[pygame.K_a]:
+        player_pos.x -= 300 * dt
+    if keys[pygame.K_d]:
+        player_pos.x += 300 * dt
     # flip() the display to put your work on screen
     pygame.display.flip()
 
