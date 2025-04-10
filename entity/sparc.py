4bcb83638623881f4163d3de1c98be2a77ecbf1e
Dylan Nguyen <dylanknguyen04@gmail.com>
2025-04-10 18:16:56 -0400

    Start of sprac mask

--- entity/sparc.py
+++ entity/sparc.py
@@ -7,3 +7,21 @@ class Sparc(Entity):
 
     def draw(self, screen):
         pygame.draw.circle(screen, "blue", (self.x, self.y), 20)
+
+    def sparc_movement(self, board_mask):
+        movement_direction = 1
+        
+        #Sparc Mask
+        newSparc = pygame.image.load("red-circle1.png").convert_alpha()
+        sparc_mask = pygame.mask.from_surface(newSparc)
+        sparc_image = sparc_mask.to_surface()
+        self.x+=movement_direction
+        self.y+=movement_direction
+        
+        qix_on_board = sparc_mask.overlap(board_mask,(self.x-1130,self.y-570))
+        if qix_on_board:
+            print("On Board")
+            
+        else:
+            print("Off Board")
+            movement_direction = movement_direction*-1
